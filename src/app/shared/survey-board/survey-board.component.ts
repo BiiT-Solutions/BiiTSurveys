2d9a25dad0961a0dbc938d57658e7d6e9047a0e2
Krystal Benlloch <krystal.benlloch@maistering.com>
2023-08-08 16:27:08 +0200

    Filtered questions with more than 2 answers

--- src/app/shared/survey-board/survey-board.component.ts
+++ src/app/shared/survey-board/survey-board.component.ts
@@ -46,7 +46,7 @@ export class SurveyBoardComponent implements OnInit{
     this.checkAuth();
     this.surveysService.getSurvey('nca').subscribe( response => {
         this.survey = CompleteFormView.clone(response);
-        this.questions = new Queue<SurveyItem>(this.survey.children[0].children);
+        this.questions = new Queue<SurveyItem>(this.survey.children[0].children.filter(c => c.children.length < 3));
         this.totalQuestions = this.questions.size();
         if (!this.questions.isEmpty()) {
           this.firstQuestion = this.questions.pop();
