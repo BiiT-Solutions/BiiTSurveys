a41fd7977ab16d6c6680be17add852e33ce54ea2
Jorge Hortelano <jhortelano@biit-solutions.com>
2023-09-26 10:10:14 +0200

    Removing console.logs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,12 +16,9 @@ export class AppComponent {
 
 
     this.getEnvironment().subscribe((_customEnvironment: any): void => {
-      console.log('$$----------------', _customEnvironment.protocol);
-      console.log('$$----------------', _customEnvironment.domain);
       if (_customEnvironment.protocol && _customEnvironment.domain) {
         Environment.ROOT_URL = `${_customEnvironment.protocol}://${_customEnvironment.domain}`;
       }
-      console.log('$$----------------', Environment.ROOT_URL);
       rootService.serverUrl = new URL(`${Environment.ROOT_URL}${Environment.KAFKA_PROXY_PATH}`);
     });
 
