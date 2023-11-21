ea46e210b114ac030e598ac7f9c76a68315c38bb
Jesus Pastor <jnavalon@biit-solutions.com>
2023-11-21 10:59:15 +0100

    changed docker variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,16 +13,6 @@ export class AppComponent {
 
 
   constructor(rootService: RootService, private http: HttpClient) {
-    this.getEnvironment().subscribe((_customEnvironment: any): void => {
-      if (_customEnvironment.protocol && _customEnvironment.domain) {
-        Environment.ROOT_URL = `${_customEnvironment.protocol}://${_customEnvironment.domain}`;
-      }
-      rootService.serverUrl = new URL(`${Environment.ROOT_URL}${Environment.KAFKA_PROXY_PATH}`);
-    });
-
-  }
-
-  getEnvironment(): Observable<any> {
-    return this.http.get<any>(`assets/environment.json`);
+    rootService.serverUrl = new URL(`${Environment.ROOT_URL}${Environment.KAFKA_PROXY_PATH}`);
   }
 }
