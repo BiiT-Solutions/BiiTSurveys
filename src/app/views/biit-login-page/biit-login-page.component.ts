d92c209a002b25e1ce756390da47cbf2df5f6ae6
kbenlloch <kbenlloch@biit-solutions.com>
2024-06-13 15:16:59 +0200

    added: reset password on login

--- src/app/views/biit-login-page/biit-login-page.component.ts
+++ src/app/views/biit-login-page/biit-login-page.component.ts
@@ -16,6 +16,7 @@ import {SessionService} from "../../services/session.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {LoginRequest, User} from "authorization-services-lib";
 import {AuthService} from "kafka-event-structure-lib";
+import {UserService} from "user-manager-structure-lib";
 
 @Component({
   selector: 'biit-login-page',
@@ -36,6 +37,7 @@ export class BiitLoginPageComponent implements OnInit {
   constructor(private authService: AuthService,
               private sessionService: SessionService,
               private biitSnackbarService: BiitSnackbarService,
+              private userService: UserService,
               biitIconService: BiitIconService,
               private activateRoute: ActivatedRoute,
               private router: Router,
@@ -107,4 +109,18 @@ export class BiitLoginPageComponent implements OnInit {
     });
   }
 
+  protected onResetPassword(email: string) {
+    this.userService.resetPassword(email).subscribe({
+      next: () => {
+        this.translocoService.selectTranslate('success', {},  {scope: 'biit-ui/login'}).subscribe(msg => {
+          this.biitSnackbarService.showNotification(msg, NotificationType.SUCCESS, null, 5);
+        });
+      },
+      error: () => {
+        this.translocoService.selectTranslate('error', {},  {scope: 'biit-ui/login'}).subscribe(msg => {
+          this.biitSnackbarService.showNotification(msg, NotificationType.ERROR, null, 5);
+        });
+      }
+    })
+  }
 }
