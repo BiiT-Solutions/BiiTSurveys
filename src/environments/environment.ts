1734fd580358411d9ec10ddd90c7ba40e4438c7b
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-11-11 10:28:43 +0100

    Releasing as open source.

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export class Environment {
+  public static ROOT_URL: string = 'https://testing.server.com';
+  public static readonly KAFKA_PROXY_PATH: string = '/kafka-proxy-backend';
+  public static readonly USER_MANAGER_SYSTEM: string = '/user-manager-system-backend';
+
+  public static SIGNUP_ALLOW = true;
+  public static SIGNUP_HIDE_PASSWORD = false;
+  public static SIGNUP_HIDE_USERNAME = false;
+  public static SIGNUP_HIDE_TEAM = false;
+}
